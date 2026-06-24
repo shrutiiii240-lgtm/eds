beafdd6d6974d4db51fcdbb1096a4cd854d16cc3
Shruti Kawadkar <shruti.k.infodales@gmail.com>
2026-06-24 13:10:43 +0530

    variant

---
+++ blocks/variant/variant.js
@@ -0,0 +1,16 @@
+export default function decorate(block) {
+  // No special code required.
+  // EDS automatically adds the option classes.
+
+  if (block.classList.contains('dark')) {
+    console.log('Dark variant enabled');
+  }
+
+  if (block.classList.contains('centered')) {
+    console.log('Centered variant enabled');
+  }
+
+  if (block.classList.contains('large')) {
+    console.log('Large variant enabled');
+  }
+}
