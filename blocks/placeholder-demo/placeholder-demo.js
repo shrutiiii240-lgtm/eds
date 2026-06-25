7b3d072f7f63e2cf23c8d179a68ae13c6a8b63e2
Shruti Kawadkar <shruti.k.infodales@gmail.com>
2026-06-25 15:22:35 +0530

    placeholder-demo added 5

--- blocks/placeholder-demo/placeholder-demo.js
+++ blocks/placeholder-demo/placeholder-demo.js
@@ -3,6 +3,7 @@ import { fetchPlaceholders } from '../../scripts/aem.js';
 export default async function decorate(block) {
   const placeholders = await fetchPlaceholders();
   console.log("placeholder= "+placeholders);
+  console.log(JSON.stringify(placeholders, null, 2));
 
   block.innerHTML = `
     <h2>${placeholders.welcome}</h2>
